6bb0d733465e76daa2a0e12d8af6b48db7c9caf5
notvv <116252693+Siriwan24@users.noreply.github.com>
2022-11-10 14:56:02 +0700

    Create Exam1_64309010014.js

---
+++ week4/Exam1_64309010014.js
@@ -0,0 +1,5 @@
+const Kelvin =293;
+let Celsius = Kelvin -273;
+let Fahrenheit = Celsius *(9/5)+32;
+Fahrenheit= Math.floor(Fahrenheit);
+console.log('The temperature is TEMPERATURE degrees Fahrenheit');
